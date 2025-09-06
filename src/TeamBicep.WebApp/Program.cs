8131b917ce2fc13cf822ebe28be3f6f060dd541d
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-09-06 23:44:39 +0200

    feat: refactor service registration and update Todos API integration

--- src/TeamBicep.WebApp/Program.cs
+++ src/TeamBicep.WebApp/Program.cs
@@ -4,26 +4,25 @@ using TeamBicep.WebApp.UI.Shared;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents().AddInteractiveServerComponents();
-builder.Services.AddScoped<IToDo, ToDoService>();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// !: Services 
+builder.Services.AddScoped<ITodosService, TodosApiService>();
 
 var apiBaseUrl =
     builder.Configuration["ApiBaseUrl"]
     ?? throw new InvalidOperationException("ApiBaseUrl configuration is missing.");
+builder.Services.AddHttpClient<ITodosService, TodosApiService>(client => client.BaseAddress = new Uri(apiBaseUrl));
 
-builder.Services.AddHttpClient<IToDo, ToDoService>(client =>
-{
-    client.BaseAddress = new Uri(apiBaseUrl);
-});
 builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-
     app.UseHsts();
 }
 
@@ -32,10 +31,16 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+
 app.UseCors(policy =>
 {
-    policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    policy
+        .AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
 });
-app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
